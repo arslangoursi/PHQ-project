0ef1a49f19c0eb25bd0e2fb97a22d48d5021dedc
Arslan <73060834+muhammadarslan478@users.noreply.github.com>
2023-01-30 15:17:38 +0500

    header changes

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -8,9 +8,18 @@ export default function Index() {
     <>
       <div className="header__container">
         <img className="header__container__img" src={PngHeaderimg} alt=""></img>
-        <div className="header__container__text">
-          <div className="header__container__text__subheading">
+        <div className="header__container__text__section">
+          <div className="header__container__text__section__subheading">
             USING PHQ BLOCKCHAIN
+          </div>
+          <div className="header__container__text__section__heading">
+            TOKENIZE ANY ASSET TANGIBLE OR NON-TANGIBLE
+          </div>
+          <div className="header__container__text__section__text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
           </div>
         </div>
       </div>
